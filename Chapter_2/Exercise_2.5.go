78b87da10736229d20012dc77013cf3eddeef754
Toringol <37870973+Toringol@users.noreply.github.com>
2019-10-24 13:58:44 +0300

    Create Exercise_2.5.go

---
+++ Chapter_2/Exercise_2.5.go
@@ -0,0 +1,53 @@
+/*
+* Выражение x&(x-1) сбрасывает крайний справа ненужный ненулевой бит x.
+* Напишите версию PopCount, которая подсчитывает биты с использованием
+* этого факта, и оцените ее производительность.
+ */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// var pc [256]byte
+
+// Example variant
+// func init() {
+// 	for i := range pc {
+// 		pc[i] = pc[i/2] + byte(i&1)
+// 	}
+// }
+
+// func PopCount(x uint64) int {
+// 	return int(pc[byte(x>>(0*8))] +
+// 		pc[byte(x>>(1*8))] +
+// 		pc[byte(x>>(2*8))] +
+// 		pc[byte(x>>(3*8))] +
+// 		pc[byte(x>>(4*8))] +
+// 		pc[byte(x>>(5*8))] +
+// 		pc[byte(x>>(6*8))] +
+// 		pc[byte(x>>(7*8))])
+// }
+
+func PopCount(x uint64) int {
+	var result int
+	for x != 0 {
+		x = x & (x - 1)
+		result++
+	}
+	return result
+}
+
+func main() {
+	start := time.Now()
+	// 94.5422ms -------- Variant with init
+	// 54.3685ms -------- Variant with drop right bit x & (x - 1)
+	for i := 0; i < 100; i++ {
+		fmt.Println(PopCount(13))
+	}
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println(elapsed)
+}
